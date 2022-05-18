18ee987de2b1a8d0a74c236ba8a96e20decae502
Aman Bele <70144726+loungeclown@users.noreply.github.com>
2022-05-18 10:20:50 +0530

    wip

---
+++ multiple games aio/games/rps/rps.py
@@ -0,0 +1,11 @@
+import random
+import tkinter
+
+rps = Tk()
+
+rps.geometry('250x250')
+rps.title('Rock Paper Scissors')
+
+rockimg = PhotoImage()
+
+rps.Label(image = )
---
+++ multiple games aio/main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+root = Tk()
+
+root.geometry('500x500')
+root.title('PyGames')
+
+
+
+
+root.mainloop()
